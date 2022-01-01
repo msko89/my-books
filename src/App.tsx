885ded303aa23daebbd4478af98553a63c25c910
msko <kms89123@gmail.com>
2022-01-02 01:39:01 +0900

    Change BrowserRouter to ConnectedRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
+import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import Add from './pages/Add';
+import Detail from './pages/Detail';
+import Edit from './pages/Edit';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Signin from './pages/Signin';
+import history from './history';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/signin' element={<Signin />} />
-        <Route path='/' element={<Home />} />
-        <Route element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path='/edit/:id' component={Edit} />
+        <Route exact path='/book/:id' component={Detail} />
+        <Route exact path='/add' component={Add} />
+        <Route exact path='/signin' component={Signin} />
+        <Route exact path='/' component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </ConnectedRouter>
   );
 }
 
